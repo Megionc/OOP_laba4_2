2c32b7b5d68091465df095346f504bf889abd7e0
megionc <lyubchenko.kseniya@gmail.com>
2022-01-24 21:57:50 +0500

    class_Model

--- OOP_laba4_2/Form1.cs
+++ OOP_laba4_2/Form1.cs
@@ -12,20 +12,17 @@ namespace OOP_laba4_2
 {
     public partial class Form1 : Form
     {
-        ModelA model1;
-        ModelB model2;
-        ModelC model3;
-
+        Model model;
+        
         public Form1()
         {
             InitializeComponent();
 
-            model1 = new ModelA();
-            model2 = new ModelB();
-            model3 = new ModelC();
-            model1.observers1 += new System.EventHandler(this.UpdateFromModel1);
-            model2.observers2 += new System.EventHandler(this.UpdateFromModel2);
-            model3.observers3 += new System.EventHandler(this.UpdateFromModel3);
+            model = new Model();
+            
+            model.observers1 += new System.EventHandler(this.UpdateFromModel1);
+            model.observers2 += new System.EventHandler(this.UpdateFromModel2);
+            model.observers3 += new System.EventHandler(this.UpdateFromModel3);
 
             trackBar1.Scroll += trackBar1_Scroll;
             trackBar2.Scroll += trackBar2_Scroll;
@@ -34,129 +31,135 @@ namespace OOP_laba4_2
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            model1.setValue(Decimal.ToInt32(trackBar1.Value));
+            model.setValue_1(Decimal.ToInt32(trackBar1.Value));
         }
 
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            model1.setValue(Decimal.ToInt32(numericUpDown1.Value));
+            model.setValue_1(Decimal.ToInt32(numericUpDown1.Value));
         }     
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
-                model1.setValue(Int32.Parse(textBox1.Text));
+                model.setValue_1(Int32.Parse(textBox1.Text));
         }
 
         private void trackBar2_Scroll(object sender, EventArgs e)
         {
-            model2.setValue(Decimal.ToInt32(trackBar2.Value));
+            model.setValue_2(Decimal.ToInt32(trackBar2.Value));
         }
 
         private void textBox2_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
-                model2.setValue(Int32.Parse(textBox2.Text));
+                model.setValue_2(Int32.Parse(textBox2.Text));
         }
 
         private void numericUpDown2_ValueChanged(object sender, EventArgs e)
         {
-            model2.setValue(Decimal.ToInt32(numericUpDown2.Value));
+            model.setValue_2(Decimal.ToInt32(numericUpDown2.Value));
         }
 
         private void trackBar3_Scroll(object sender, EventArgs e)
         {
-            model3.setValue(Decimal.ToInt32(trackBar3.Value));
+            model.setValue_3(Decimal.ToInt32(trackBar3.Value));
         }
 
         private void textBox3_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Enter)
-                model3.setValue(Int32.Parse(textBox3.Text));
+                model.setValue_3(Int32.Parse(textBox3.Text));
         }
 
         private void numericUpDown3_ValueChanged(object sender, EventArgs e)
         {
-            model3.setValue(Decimal.ToInt32(numericUpDown3.Value));
+            model.setValue_3(Decimal.ToInt32(numericUpDown3.Value));
         }
 
         private void UpdateFromModel1(object sender, EventArgs e)
         {
-            textBox1.Text = model1.getValue().ToString();
-            numericUpDown1.Value = model1.getValue();
-            progressBar1.Value = model1.getValue();
-            trackBar1.Value = Decimal.ToInt32(model1.getValue());                
+            textBox1.Text = model.getValue_1().ToString();
+            numericUpDown1.Value = model.getValue_1();
+            progressBar1.Value = model.getValue_1();
+            trackBar1.Value = Decimal.ToInt32(model.getValue_1());                
         }
 
         private void UpdateFromModel2(object sender, EventArgs e)
         {
-            textBox2.Text = model2.getValue().ToString();
-            numericUpDown2.Value = model2.getValue();
-            progressBar2.Value = model2.getValue();
-            trackBar2.Value = Decimal.ToInt32(model2.getValue());
+            textBox2.Text = model.getValue_2().ToString();
+            numericUpDown2.Value = model.getValue_2();
+            progressBar2.Value = model.getValue_2();
+            trackBar2.Value = Decimal.ToInt32(model.getValue_2());
         }
 
         private void UpdateFromModel3(object sender, EventArgs e)
         {
-            textBox3.Text = model3.getValue().ToString();
-            numericUpDown3.Value = model3.getValue();
-            progressBar3.Value = model3.getValue();
-            trackBar3.Value = Decimal.ToInt32(model3.getValue());
+            textBox3.Text = model.getValue_3().ToString();
+            numericUpDown3.Value = model.getValue_3();
+            progressBar3.Value = model.getValue_3();
+            trackBar3.Value = Decimal.ToInt32(model.getValue_3());
         }
 
         
     }
 
 
-    public class ModelA
+    public class Model
     {
-        private int value_1;
+        private int value_1= 10, value_2=50, value_3=90;
+
         public System.EventHandler observers1;
-        
-        public void setValue(int value_1)
-        {
-            if ((value_1 >= 0) & (value_1 <= 100))
-                this.value_1 = value_1;
-            
-            observers1.Invoke(this, null);            
-        }
-        public int getValue()
-        {
-            return value_1;
-        }
-    }
-
-    public class ModelB
-    {
-        private int value_2;       
         public System.EventHandler observers2;
-
-        public void setValue(int value_2)
-        {
-            if ((value_2 > value_1) && (value_2 < value_3))
-                this.value_2 = value_2;
-            
-            observers2.Invoke(this, null);
-        }
-        public int getValue()
-        {
-            return value_2;
-        }
-    }
-
-    public class ModelC
-    {
-        private int value_3;
         public System.EventHandler observers3;
 
-        public void setValue(int value_3)
+        public void setValue_1(int value_1)
         {
-            if ((value_3 >= 0) & (value_3 <= 100))
+            if (value_1 >= 0 && value_1 < value_2)
+                this.value_1 = value_1;
+            else if (value_1 >= value_2)
+                this.value_1 = value_1 - 1;
+            else if (value_1 < 0)
+                this.value_1 = 0;
+
+            observers1.Invoke(this, null);            
+        }
+        public int getValue_1()
+        {
+            return value_1;
+        }    
+        
+
+        public void setValue_2(int value_2)
+        {
+            if (value_2 > value_1 && value_2 < value_3)
+                this.value_2 = value_2;
+            else if (value_2 <= value_1)
+                this.value_2 = value_2 + 1;
+            else if (value_2 >= value_3)
+                this.value_2 = value_2 - 1;
+
+            observers2.Invoke(this, null);
+        }
+        public int getValue_2()
+        {
+            return value_2;
+        }     
+
+
+        public void setValue_3(int value_3)
+        {
+
+            if (value_3 > value_2 && value_3 <= 100)
                 this.value_3 = value_3;
-            
+            else if (value_3 <= value_2)
+                this.value_3 = value_3 + 1;
+            else if (value_3 > 100)
+                this.value_3 = 100;
+
             observers3.Invoke(this, null);
         }
-        public int getValue()
+        public int getValue_3()
         {
             return value_3;
         }
